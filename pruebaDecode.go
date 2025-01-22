4aaa5047c4d76df71b54d66e40ff6bd9201a47a1
Alejandro Salas <joal.sala@gmail.com>
2025-01-22 18:06:44 -0500

    asd

---
+++ pruebaDecode.go
@@ -0,0 +1,19 @@
+package main
+
+// import (
+// 	"encoding/base64"
+// 	"fmt"
+// )
+
+// func main() {
+
+// 	encodedStr := "ZGVkaWNhY2nDs24="
+
+// 	decodedStr, err := base64.StdEncoding.DecodeString(encodedStr)
+// 	if err != nil {
+// 		fmt.Println("Error al decodificar:", err)
+// 		return
+// 	}
+
+// 	fmt.Println(string(decodedStr))
+// }
